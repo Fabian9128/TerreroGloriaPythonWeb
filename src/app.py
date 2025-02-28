ae9efcaba72810066ae8b55d583ad7a7c8e2a8ac
Fabian Gonzalez <fabian.gonzalez@sopasteria.com>
2025-02-28 15:09:36 +0100

    tabla rellena y boton guardar

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import os
-import database as db
+from database import Clasificacion
 
 ####################################################################
 #Código sacado del video https://www.youtube.com/watch?v=Zfpbnmdi-pE
@@ -12,11 +12,29 @@ template_dir = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
 template_dir = os.path.join(template_dir, 'src', 'templates')
 #Variable flask
 app = Flask(__name__, template_folder = template_dir)
-#Rutas de la aplicación
+# Instanciar la base de datos
+jugadores = Clasificacion()
+
+#Ruta vista inicial
 @app.route('/')
 def home():
+    #Obtener datos de la clasificacion
+    clasificacion = jugadores.consulta_clasificacion()
+    #Sacar los datos
+    return render_template('index.html', clasificacion=clasificacion)
 
-    return render_template('index.html')
+#Ruta guardar jugador nuevo
+@app.route('/jugador', methods=['POST'])
+def guardarJugador():
+    #Obtener valores introducidos
+    nombre = request.form['nombre']
+    puntos = request.form['puntos']
+    exacto = request.form['exacto']
+    dobles = request.form['dobles']
+    #Añadir jugador
+    jugadores.inserta_jugador(nombre,puntos,exacto, dobles, "Insular")
+    #Sacar los datos
+    return redirect(url_for('home'))
 
 #Lanzar la app
 if __name__ == '__main__':
--- src/database.py
+++ src/database.py
@@ -1,4 +1,103 @@
 import pymysql
 
-# Conexión a la base de datos
-miConexcion = pymysql.connect(host="localhost", user="root", passwd="Mariana_9128", database="prueba")
+class Clasificacion:
+
+    def __init__(self):
+        # Conexión a la base de datos
+        self.miConexcion = pymysql.connect(host="localhost", user="root", passwd="Mariana_9128", database="prueba")
+
+    def consulta_clasificacion(self):
+        cur = self.miConexcion.cursor()
+        cur.execute("SELECT * FROM clasificacion")
+        datos = cur.fetchall()     
+        cur.close()
+
+        return datos
+    
+    def ordena_clasificacion(self, columna_puntos, columna_exacto):
+
+        cur = self.miConexcion.cursor()
+        #Obtener datos ordenados por puntos
+        consulta_sql = f"""
+        SELECT * FROM clasificacion 
+        ORDER BY {columna_puntos} DESC, {columna_exacto} DESC
+        """
+        cur.execute(consulta_sql)
+        datos = cur.fetchall()
+        #Borrar todos los datos de la tabla original
+        cur.execute("DELETE FROM clasificacion")
+        #Insertar los datos en el nuevo orden
+        for fila in datos:
+            sql_insert = '''
+            INSERT INTO clasificacion (Nombre, Puntos_Insular, Exacto_Insular, Dobles_Insular, Puntos_Regional, Exacto_Regional, Dobles_Regional, Puntos_General, Exacto_General, Dobles_General) 
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            '''
+            cur.execute(sql_insert, fila)
+
+        self.miConexcion.commit()     
+        cur.close()
+
+        return datos
+
+    def inserta_jugador(self, valor_nombre, valor_puntos, valor_exacto, valor_dobles, seleccion):
+
+        cur = self.miConexcion.cursor()
+        #Ejecuta la consulta insertando los valores dependiendo de la seleccion
+        if seleccion == "Insular":
+            sql_insert = '''
+            INSERT INTO clasificacion (Nombre, Puntos_Insular, Exacto_Insular, Dobles_Insular) 
+            VALUES (%s, %s, %s, %s)
+            '''
+        else:
+            sql_insert = '''
+            INSERT INTO clasificacion (Nombre, Puntos_Regional, Exacto_Regional, Dobles_Regional) 
+            VALUES (%s, %s, %s, %s)
+            '''
+        #Ejecuta la consulta pasando los valores como parámetros
+        cur.execute(sql_insert, (valor_nombre, valor_puntos, valor_exacto, valor_dobles))
+        #Confirma los cambios
+        self.miConexcion.commit()
+        cur.close()
+        #Devuelve el número de filas afectadas
+        return cur.rowcount
+
+    def elimina_jugador(self, valor_nombre):
+
+        cur = self.miConexcion.cursor()
+        #Ejecuta la consulta eliminando por nombre
+        sql='''DELETE FROM clasificacion WHERE Nombre = %s'''
+        cur.execute(sql, (valor_nombre))
+        #Confirma los cambios
+        self.miConexcion.commit()
+        cur.close()
+        #Devuelve el número de filas afectadas
+        return cur.rowcount
+    
+    def busca_jugador(self, valor_nombre):
+
+        cur = self.miConexcion.cursor()
+        #Ejecuta la consulta buscando por nombre
+        sql='''SELECT * FROM clasificacion WHERE Nombre = %s'''
+        cur.execute(sql, (valor_nombre))
+        #Guarda la selección como resultado
+        resultado = cur.fetchone()
+        cur.close()
+        #Devuelve si encontró al jugador
+        return resultado is not None
+    
+    def modifica_jugador(self, valor_nombre, valor_puntos, valor_exacto, valor_dobles, seleccion):
+
+        cur = self.miConexcion.cursor()
+        #Ejecuta la consulta modificando los valores dependiendo de la seleccion
+        if seleccion == "Insular":
+            sql='''UPDATE clasificacion SET Puntos_Insular = %s, Exacto_Insular = %s, Dobles_Insular = %s WHERE Nombre = %s'''
+        elif seleccion == "Regional":
+            sql='''UPDATE clasificacion SET Puntos_Regional = %s, Exacto_Regional = %s, Dobles_Regional = %s WHERE Nombre = %s'''
+        else:
+            sql='''UPDATE clasificacion SET Puntos_General = %s, Exacto_General = %s, Dobles_General = %s WHERE Nombre = %s'''
+        cur.execute(sql, (valor_puntos, valor_exacto, valor_dobles, valor_nombre))
+        #Confirma los cambios
+        self.miConexcion.commit()
+        cur.close()
+        #Devuelve el número de filas afectadas
+        return cur.rowcount
